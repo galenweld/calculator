db81f6e7344c3e70eedeb5e5a1987e15b997906a
Galen Weld <galen.weld@gmail.com>
2015-11-14 01:44:19 -0500

    added functions to convert string to token stream

--- parse.ml
+++ parse.ml
@@ -3,15 +3,26 @@ open Expr
 open Eval
 
 
-let text_to_word_list (s : bytes) : bytes list =
-	let rec text_to_word_list_helper t = 
-		if contains t ' ' then
-			let hd = sub t 0 (index t ' ') in
-			let tl = sub t ((index t ' ') +1) ((length t) - ((index t ' ') +1)) in
-			if hd = "" then text_to_word_list_helper tl else
-				hd::(text_to_word_list_helper tl)
-		else [t]
-	in text_to_word_list_helper (trim s)
+let rec text_to_char_list t =
+	match t with
+	| "" -> []
+	| _  -> (get t 0)::(text_to_char_list (sub t 1 ((length t) -1)))
+
+
+type token = Int of int | Plus | Minus | Times | OParen | CParen
+
+
+let char_list_to_token_list (s : char list) : token list =
+	let rec fold_helper l c = 
+		match c with
+		| ' ' -> l@ []
+		| '(' -> l@ [OParen]
+		| ')' -> l@ [CParen]
+		| '+' -> l@ [Plus]
+		| '-' -> l@ [Minus]
+		| '*' -> l@ [Times]
+		|  x  -> l@ [Int (int_of_string (Bytes.make 1 x))]
+	in List.fold_left fold_helper [] s
 
 
 let parse (s:bytes) : expr =
