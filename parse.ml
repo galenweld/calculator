a13922044370eee4c56d7fa119d22a65cfa1562c
Galen Weld <galen.weld@gmail.com>
2015-11-14 00:52:39 -0500

    added helpers to parse

--- parse.ml
+++ parse.ml
@@ -2,5 +2,17 @@ open Bytes
 open Types
 open Eval
 
+
+let text_to_word_list (s : bytes) : bytes list =
+	let rec text_to_word_list_helper t = 
+		if contains t ' ' then
+			let hd = sub t 0 (index t ' ') in
+			let tl = sub t ((index t ' ') +1) ((length t) - ((index t ' ') +1)) in
+			if hd = "" then text_to_word_list_helper tl else
+				hd::(text_to_word_list_helper tl)
+		else [t]
+	in text_to_word_list_helper (trim s)
+
+
 let parse (s:bytes) : expr =
 	blah
